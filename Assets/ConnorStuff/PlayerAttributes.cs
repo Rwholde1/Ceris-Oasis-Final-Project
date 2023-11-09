401e6a01db56a213d9e3d83142a10a1e91e7d4d2
DelishPopcornGames <cdonihi@asu.edu>
2023-11-09 14:59:39 -0700

    import of old enemy stuff

---
+++ Assets/ConnorStuff/PlayerAttributes.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float health = 100f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void Damage(float damage)
+    {
+        health -= damage;
+    }
+}
