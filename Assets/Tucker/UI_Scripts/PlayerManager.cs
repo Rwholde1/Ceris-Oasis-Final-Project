911b9893f3e1eebd18b23ac71fd9549a7d155273
Tucker Wood <42354895+polygeist111@users.noreply.github.com>
2023-11-10 15:21:47 -0700

    First Big UI Pass
    All UI changes currently local to UI Test Scene
    - Adds pause menu (esc key)
    - Adds working healthbar (press k to deduct 10 health for testing), as well as player controller script
    - Adds explosive barrel prefab for level design use
    - Adds empty main menu scene (and puts it in build settings)

--- Assets/Tucker/UI_Scripts/PlayerManager.cs
+++ Assets/Tucker/UI_Scripts/PlayerManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
+
+    [SerializeField] int maxHealth = 100;
+    private int currentHealth;
+
+    private bool alive = true;
+    [SerializeField] object primaryWeapon;
+    [SerializeField] object secondaryWeapon;
+    public HealthBar healthBar;
+
+    //grenades
+    //shielding?
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentHealth <= 0) {
+            alive = false;
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.K)) {
+            takeDamage(10);
+        }
+
     }
+
+    void takeDamage(int damageIn) {
+        currentHealth -= damageIn;
+        healthBar.setHealth(currentHealth);
+    }
+
 }
