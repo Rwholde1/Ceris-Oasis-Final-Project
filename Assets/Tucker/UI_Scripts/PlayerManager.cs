bdc6c92c50b02922854b3b66059625bb3a086b38
Tucker Wood <42354895+polygeist111@users.noreply.github.com>
2023-11-10 20:02:20 -0700

    Ping System, URP Render Settings

--- Assets/Tucker/UI_Scripts/PlayerManager.cs
+++ Assets/Tucker/UI_Scripts/PlayerManager.cs
@@ -18,6 +18,9 @@ public class PlayerManager : MonoBehaviour
     public HealthBar healthBar;
     public AbilityManager abilities;
 
+    public Camera mainCam;
+    public GameObject basicPing;
+
     //grenades
     //shielding?
 
@@ -43,11 +46,32 @@ public class PlayerManager : MonoBehaviour
             takeDamage(10);
         }
 
+        if(Input.GetMouseButtonDown(2)) {
+            createPing();
+        }
+
     }
 
     void takeDamage(int damageIn) {
         currentHealth -= damageIn;
         healthBar.setHealth(currentHealth);
+    }
+
+    void createPing() {
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+                    Debug.Log("ping");
+        if(Physics.Raycast(ray, out RaycastHit hit)) {
+                        Debug.Log("ping1");
+            //Sets height offset and instantiates ping
+            Vector3 offset = new Vector3 (hit.point.x, hit.point.y + 0.1f, hit.point.z);
+            Instantiate(basicPing, offset, Quaternion.identity);
+
+            
+            if (hit.collider.gameObject.layer == 8) {
+                            Debug.Log("ping2");
+                
+            }
+        }
     }
 
 }
