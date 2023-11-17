ab50a59f514ea400f82407c3b5eca5a325bb540b
ConnorDonihi <cdonihi@asu.edu>
2023-11-16 17:17:12 -0700

    AIStuff
    More AI state stuff
    can change target, flee properly, and not flee for too long. active chase and attacks+attack radius coming

--- Assets/AIStuff/AI_Gen_State.cs
+++ Assets/AIStuff/AI_Gen_State.cs
@@ -10,18 +10,21 @@ public class AI_Gen_State : MonoBehaviour
         CHASE = 1,
         FLEE = 2,
         WAIT = 0,
-        ATTACK = 3
+        ATTACK = 3,
+        ACTIVECHASE = 4,
     }
     [SerializeField] AI_STATE state;
     private Transform enemyT;
     private float speed = 1f;
 
+    GameObject targetObject;
 
     private UnityEngine.AI.NavMeshAgent agent;
     private Vector3 targetPos;
     void Start()
     {
         state = AI_STATE.CHASE;
+        agent = GetComponent<NavMeshAgent>();
         enemyT = GetComponent<Transform>();
     }
     
@@ -40,48 +43,74 @@ public class AI_Gen_State : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch(state)
-    {
-        case AI_STATE.CHASE:
-            Debug.Log("Chasing");
-            break;
+        agent.destination = targetPos;
+        switch (state)
+        {
+            case AI_STATE.CHASE:
+                { 
+                Debug.Log("Chasing to target");
+                break; 
+                }
+            case AI_STATE.ACTIVECHASE:
+                {
+                    Debug.Log("ACTIVELY Chasing");
+
+                    break;
+                }
+            case AI_STATE.FLEE:
         
-        case AI_STATE.FLEE:
+                Debug.Log("Fleeing");
+                Vector3 diff = new Vector3();
+                diff = enemyT.position- targetPos;
+                diff.Normalize();
+                diff *= 15;
+
+                targetPos = targetPos+ diff;
+                StartCoroutine(FleeTimer());
+                state = AI_STATE.CHASE;
+                break;
         
-            Debug.Log("Fleeing");
-            break;
+            case AI_STATE.WAIT:
         
-        case AI_STATE.WAIT:
+                Debug.Log("Waiting");
+                targetPos = enemyT.position;
+
+                break;
         
-            Debug.Log("Waiting");
-            break;
+            case AI_STATE.ATTACK:
         
-        case AI_STATE.ATTACK:
+                Debug.Log("Attacking");
+                Debug.Log("Right now changes state to chase player");
+                ChangeTarget("Player");
+                state = AI_STATE.CHASE;
+                break;
         
-            Debug.Log("Attacking");
-            break;
-        
-        default:
-            Debug.Log("NOTHING)");
-            break;
-    }  
+            default:
+                Debug.Log("NOTHING)");
+                break;
+        }  
     }
     public void ChangeTarget()
     {
-        //change the AI target
+        
     }
     public void ChangeTarget(string theTag)
     {
         //change the AI target based on the tag
-        GameObject tempObject = GameObject.FindWithTag(theTag);
-        Vector3 tempPos = tempObject.GetComponent<Transform>().position;
+        targetObject = GameObject.FindWithTag(theTag);
+        Vector3 tempPos = targetObject.GetComponent<Transform>().position;
         targetPos = tempPos;
-        
     }
     void AIChasePlayer()
     {
         //moves towards the target
 
+    }
+
+    IEnumerator FleeTimer()
+    {
+        yield return new WaitForSeconds(5f);
+        state = AI_STATE.ATTACK;
     }
     void AIWait()
     {
