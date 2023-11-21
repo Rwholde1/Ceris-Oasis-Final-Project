10fcbbf5a2f09fbfa77e367a1a8cc0922e9a0399
ConnorDonihi <cdonihi@asu.edu>
2023-11-21 02:10:00 -0700

    Raycasting Player detection maybe?
    Not quite working yet I am so close though

--- Assets/AIStuff/AI_Gen_State.cs
+++ Assets/AIStuff/AI_Gen_State.cs
@@ -13,6 +13,11 @@ public class AI_Gen_State : MonoBehaviour
         ATTACK = 3,
         ACTIVECHASE = 4,
     }
+
+    public bool attackWhenClose;
+    public float ADT; //Attack Distance Threshold
+
+
     [SerializeField] AI_STATE state;
     private Transform enemyT;
     private float speed = 1f;
@@ -23,19 +28,13 @@ public class AI_Gen_State : MonoBehaviour
     private Vector3 targetPos;
     void Start()
     {
-        state = AI_STATE.CHASE;
+        //state = AI_STATE.CHASE;
         agent = GetComponent<NavMeshAgent>();
         enemyT = GetComponent<Transform>();
+        attackWhenClose = true;
+        targetPos = targetObject.transform.position;
     }
     
-    public int CheckState()
-    {
-        return (int)state;
-    }
-    float CheckDistance()
-    {
-    return 0f;
-    }
     public void ChangeSpeed(float newSpeed)
     {
         speed = newSpeed;
@@ -47,14 +46,23 @@ public class AI_Gen_State : MonoBehaviour
         switch (state)
         {
             case AI_STATE.CHASE:
-                { 
-                Debug.Log("Chasing to target");
+                {
+                    Debug.Log("Chasing to target");
+
+                    if (CastToPlayer())
+                    { 
+                    ChangeTarget("Player");
+                    state = AI_STATE.ACTIVECHASE;
+                    }
                 break; 
                 }
             case AI_STATE.ACTIVECHASE:
                 {
                     Debug.Log("ACTIVELY Chasing");
-
+                    if(CheckDistance()<= ADT)
+                    {
+                        state= AI_STATE.ATTACK;
+                    }
                     break;
                 }
             case AI_STATE.FLEE:
@@ -67,7 +75,7 @@ public class AI_Gen_State : MonoBehaviour
 
                 targetPos = targetPos+ diff;
                 StartCoroutine(FleeTimer());
-                state = AI_STATE.CHASE;
+                state = AI_STATE.ACTIVECHASE;
                 break;
         
             case AI_STATE.WAIT:
@@ -78,11 +86,20 @@ public class AI_Gen_State : MonoBehaviour
                 break;
         
             case AI_STATE.ATTACK:
-        
+                if (CheckDistance() >= ADT && !CastToPlayer())
+                {
+                    state = AI_STATE.CHASE;
+                }
                 Debug.Log("Attacking");
                 Debug.Log("Right now changes state to chase player");
+                AIAttack();
+
+                //Updates player pos
                 ChangeTarget("Player");
-                state = AI_STATE.CHASE;
+                
+                
+                
+                //state = AI_STATE.CHASE;
                 break;
         
             default:
@@ -90,6 +107,31 @@ public class AI_Gen_State : MonoBehaviour
                 break;
         }  
     }
+
+    public int CheckState() ///method to return the current state
+    {
+        return (int)state;
+    }
+    public bool CastToPlayer()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(enemyT.position, (2*enemyT.position) - CheckTarget("Player"), out hit, 50f, ~0))
+        {
+            if (hit.transform.CompareTag("Player"))
+            {
+                print("Player hit raycasted");
+                return true;
+            }
+        }
+        print("Player Not HIT");
+        return false;
+    }
+
+    float CheckDistance() //method to return the distance between the target and the current position
+    {
+        return Mathf.Abs(Vector3.Magnitude(enemyT.position - targetPos));
+    }
+
     public void ChangeTarget()
     {
         
@@ -100,6 +142,11 @@ public class AI_Gen_State : MonoBehaviour
         targetObject = GameObject.FindWithTag(theTag);
         Vector3 tempPos = targetObject.GetComponent<Transform>().position;
         targetPos = tempPos;
+    }
+    public Vector3 CheckTarget(string theTag)
+    {
+        //return the AI target POSITION based on the tag
+        return GameObject.FindWithTag(theTag).GetComponent<Transform>().position;
     }
     void AIChasePlayer()
     {
