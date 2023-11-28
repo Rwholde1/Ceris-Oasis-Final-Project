6778da188d6e9ce99ae54303383fee74f441509c
heejinsoul <haliliaden@gmail.com>
2023-11-28 00:50:32 -0700

    level3Design Finished

---
+++ Assets/Aden/Vladislav Simakov/AnimationStuff/BusDoorLeftController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusDoorLeftController : MonoBehaviour
+{
+    [SerializeField] private Animator Door = null;
+
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!openTrigger)
+            {
+                Door.Play("DoorOpen",0,0.0f);
+                gameObject.SetActive(false);
+            }
+
+            else if(closeTrigger)
+            {
+                Door.Play("DoorClose", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
