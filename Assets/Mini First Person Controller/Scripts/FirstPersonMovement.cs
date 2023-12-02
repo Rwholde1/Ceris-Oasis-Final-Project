a4f6041d580c518eefd45e1fd153a939b3074b6d
whatiswhatlol <bradydun29@gmail.com>
2023-12-02 16:31:07 -0700

    PIRATE ARRRGGG

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -11,6 +11,7 @@ public class FirstPersonMovement : MonoBehaviour
     public float runSpeed = 9;
     public KeyCode runningKey = KeyCode.LeftShift;
     public Animator animator;
+    public bool isMovementEnabled = true;
 
     Rigidbody rigidbody;
     /// <summary> Functions to override movement speed. Will use the last added override. </summary>
@@ -26,73 +27,80 @@ public class FirstPersonMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift))
+        if (isMovementEnabled)
         {
-            //walking forward
-            animator.SetBool("W", true);
-            animator.SetBool("A", false);
-            animator.SetBool("S", false);
-            animator.SetBool("D", false);
+            if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.LeftShift))
+            {
+                //walking forward
+                animator.SetBool("W", true);
+                animator.SetBool("A", false);
+                animator.SetBool("S", false);
+                animator.SetBool("D", false);
 
-        }
-        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
-        {
-            animator.SetBool("Shift", true);
-            animator.SetBool("W", true);
-            animator.SetBool("A", false);
-            animator.SetBool("S", false);
-            animator.SetBool("D", false);
-            //walking back
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            animator.SetBool("Shift", false);
-            animator.SetBool("W", false);
-            animator.SetBool("A", false);
-            animator.SetBool("S", true);
-            animator.SetBool("D", false);
-            //walking back
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            animator.SetBool("Shift", false);
-            animator.SetBool("W", false);
-            animator.SetBool("A", false);
-            animator.SetBool("S", false);
-            animator.SetBool("D", true);
-            //walking right
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            animator.SetBool("Shift", false);
-            animator.SetBool("W", false);
-            animator.SetBool("A", true);
-            animator.SetBool("S", false);
-            animator.SetBool("D", false);
-            //walking left
-        }
-        else
-        {
-            animator.SetBool("Shift", false);
-            animator.SetBool("W", false); ;
-            animator.SetBool("A", false);
-            animator.SetBool("S", false);
-            animator.SetBool("D", false);
-        }
-        // Update IsRunning from input.
-        IsRunning = canRun && Input.GetKey(runningKey);
+            }
+            else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+            {
+                animator.SetBool("Shift", true);
+                animator.SetBool("W", true);
+                animator.SetBool("A", false);
+                animator.SetBool("S", false);
+                animator.SetBool("D", false);
+                //walking back
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                animator.SetBool("Shift", false);
+                animator.SetBool("W", false);
+                animator.SetBool("A", false);
+                animator.SetBool("S", true);
+                animator.SetBool("D", false);
+                //walking back
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                animator.SetBool("Shift", false);
+                animator.SetBool("W", false);
+                animator.SetBool("A", false);
+                animator.SetBool("S", false);
+                animator.SetBool("D", true);
+                //walking right
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                animator.SetBool("Shift", false);
+                animator.SetBool("W", false);
+                animator.SetBool("A", true);
+                animator.SetBool("S", false);
+                animator.SetBool("D", false);
+                //walking left
+            }
+            else
+            {
+                animator.SetBool("Shift", false);
+                animator.SetBool("W", false); ;
+                animator.SetBool("A", false);
+                animator.SetBool("S", false);
+                animator.SetBool("D", false);
+            }
+            // Update IsRunning from input.
+            IsRunning = canRun && Input.GetKey(runningKey);
 
-        // Get targetMovingSpeed.
-        float targetMovingSpeed = IsRunning ? runSpeed : speed;
-        if (speedOverrides.Count > 0)
-        {
-            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+            // Get targetMovingSpeed.
+            float targetMovingSpeed = IsRunning ? runSpeed : speed;
+            if (speedOverrides.Count > 0)
+            {
+                targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+            }
+
+            // Get targetVelocity from input.
+            Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+
+            // Apply movement.
+            rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
         }
-
-        // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
-
-        // Apply movement.
-        rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+    }
+    public void SetMovementEnabled(bool isEnabled)
+    {
+        isMovementEnabled = isEnabled;
     }
 }
